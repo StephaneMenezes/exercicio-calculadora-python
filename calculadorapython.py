0fd623242c3d440ed337bbbe8f7fe390afae0217
StephaneMenezes <72771600+StephaneMenezes@users.noreply.github.com>
2021-10-22 09:49:01 -0300

    Update calculadorapython.py

--- calculadorapython.py
+++ calculadorapython.py
@@ -1,4 +1,6 @@
-print("************Bem-vindo a calculadora Python**************")
+import math
+
+print('*'*11, "Bem-vindo a calculadora Python",'*'*11)
 def adicionar(x,y):
     return x + y 
 def subtrair(x,y):
@@ -7,13 +9,15 @@ def multiplicar(x,y):
     return x * y 
 def dividir(x,y):
     return x / y 
+def raiz(x,y):
+    return math.sqrt(x), math.sqrt(y)
 
 print(" Escolha a função que deseja utilizar: ")
 print("1 - Soma ")
 print("2 - Subtração ")
 print("3 - Multiplicação ")
 print("4 - Divisão ")
-
+print("5 - Raíz quadrada")
 escolha = input("Digite a operação que deseja executar: ")
 
 num1 = int(input("Digite o primeiro número: "))
@@ -27,6 +31,8 @@ elif escolha == "3":
     print(num1,"*", num2, "=", multiplicar(num1,num2))
 elif escolha == "4": 
     (num1,"/", num2, "=", dividir(num1,num2))
+elif escolha == "5":
+    print("Raiz quadrada de: ", num1, "e", num2, "é: ", raiz(num1,num2))
 else: 
     print("Opção inválida, por favor escolha uma operação válida. ")
 
